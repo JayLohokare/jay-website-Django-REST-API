6b0a58100966704413a3ada113d4f44b0a22f706
JayLohokare <jaylohokare@gmail.com>
2018-08-05 19:34:45 -0600

    Removed Cors

--- jayWebsite/settings.py
+++ jayWebsite/settings.py
@@ -60,7 +60,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'projects.middleWare.corsMiddleware'
+    
    
 ]
 
