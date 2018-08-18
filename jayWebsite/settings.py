c6484ccde0c26f4f358f5f35383972eefab5d97f
jaylohokare <jaylohokare@gmail.com>
2018-08-18 18:38:06 -0400

    Added static file setting for production

--- jayWebsite/settings.py
+++ jayWebsite/settings.py
@@ -134,4 +134,9 @@ CORS_ORIGIN_ALLOW_ALL = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# The URL to use when referring to static files (where they will be served from)
 STATIC_URL = '/static/'
